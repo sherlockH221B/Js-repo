f2f5cf0771158bfd215b76bf757420781bff1232
sherlockH221B <tusharyeolekar98@gmail.com>
2024-07-18 22:01:22 +0530

    Datatypes

--- test.js
+++ test.js
@@ -1,1 +1,52 @@
-console.log("Tushar")
+console.log("Sherlock")
+
+
+//  Video 04 Variables
+
+const accountId = 144553
+let accountEmail = "yeolekart@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+let accountState;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+{
+    //Scope basis.
+}
+
+// use let and const 
+// prefer not to use var due to functional Scope and 
+
+
+// Lecture 05 DataTypes 
+
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3) //we are using nodejs not browser
+
+console.log(3 + 3); 
+console.log("Sherlock")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique figma tools uniqueness
+
+/* Object */
+console.log(typeof "Sherock");
+console.log(typeof null)
+console.log(typeof undefined)
