46bd05e08be9f86e4bdf2d3ab5c12813dc788987
sherlockH221B <tusharyeolekar98@gmail.com>
2024-07-19 22:25:48 +0530

    Lecture 9

---
+++ lect9.js
@@ -0,0 +1,36 @@
+//Datatypes of JS
+// Js is Dynamically Type language() 
+
+//premitive 
+//(7) String, Number, Boolean, Null, Undefined, Symbol, BigInt.
+
+const score = 100;
+const scoreValue = 100.3
+const isLoggedIn = false
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber = 3416849684651321654684684n
+
+
+
+//non-premitive(Refrence Type)
+//(3) Array, Objects, Functions.
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Sherlock",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof scoreValue);
+
+
+// Lect 10 
---
+++ lecture_7.js
@@ -0,0 +1,53 @@
+// Operations 
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+//console.log(2+2);
+// console.log(2-2);
+
+
+let str1 = "Hello";
+let str2 = "world";
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+//prefix and postfix operator
+
+
+//Lecture8
+
+//Compression
+
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 != 1);
+console.log(2 == 1);
+
+
+//console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+// === strict check
+
+console.log("2" == 2);
+console.log("2" === 2);
