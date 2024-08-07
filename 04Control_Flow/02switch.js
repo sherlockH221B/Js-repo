12c0a6c03b57a9a18770630358b49b7a4127a7ba
sherlockH221B <tusharyeolekar98@gmail.com>
2024-08-07 23:35:11 +0530

    Switch

---
+++ 04Control_Flow/02switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//const month = 3
+//switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+
+//     default:
+//         console.log("Default");
+//         break;
+//         // It run entire code after getting matched except default
+// }
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("Mar");
+        break;
+    case "apr":
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Default");
+        break;
+        // It run entire code after getting matched except default
+}
+
+
