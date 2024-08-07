dfb939d4dab4d9a322bbb8680ce838776bf908fa
sherlockH221B <tusharyeolekar98@gmail.com>
2024-08-07 23:56:46 +0530

    Till 35-38 Min Video No. 26

---
+++ 04Control_Flow/03truthy.js
@@ -0,0 +1,18 @@
+const
+const userEmail = "s@herlock.com"
+
+if (userEmail) {
+    console.log("Got user Email");          
+} else {
+    console.log("Dont have user email");    
+}
+
+// Falsy Value 
+//false, 0, -0, BigInt 0n, "", null, undefined, NaN.
+
+//Truthy Value
+// "0", 'false', " ", [], {}, function(){},
+
+
+// Nullish Coalescing Operator (??): null undefined
+
