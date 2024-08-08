91f1a47514f20ab8f8a19ea03c344d29a20d2435
sherlockH221B <tusharyeolekar98@gmail.com>
2024-08-08 23:09:40 +0530

    note: till video no. 26 ; for summarize video start watching from 48 min to last

--- 04Control_Flow/03truthy.js
+++ 04Control_Flow/03truthy.js
@@ -1,4 +1,3 @@
-const
 const userEmail = "s@herlock.com"
 
 if (userEmail) {
@@ -15,4 +14,24 @@ if (userEmail) {
 
 
 // Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 50
+
+console.log(val1);
+console.log(val1);
+console.log(val1);
+
+//++++++++++++++++++++++++++++++++++++++++++++
+
+// Terniary Operator
+//condition ? true : false
+
+const iceTeaprice = 100
+iceTeaprice <= 80 ? console.log("Less then 80") : console.log("More than 80");
+
+
 
