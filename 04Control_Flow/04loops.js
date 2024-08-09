d5bf96688395aa5577bd9bffa72201fef3a991b7
sherlockH221B <tusharyeolekar98@gmail.com>
2024-08-09 22:39:08 +0530

    Video No. 27 For Loop

---
+++ 04Control_Flow/04loops.js
@@ -0,0 +1,86 @@
+// FOR LOOPS 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is the best number");  
+//     }
+//     console.log(element);
+    
+// }
+
+//console.log(element);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         //console.log(`inner loop value ${j} and inner value ${i}`);  
+//         console.log(i + '*' + j + ' = '  + i*j);
+              
+//     }
+    
+// }
+
+// let myArray = ["Flash", "Batman", "Superman"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
