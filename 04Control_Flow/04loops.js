36ebb969b793333f9f71512127d84782b5641513
sherlockH221B <tusharyeolekar98@gmail.com>
2024-08-10 22:27:33 +0530

    While Loop / Do While Loop

--- 04Control_Flow/04loops.js
+++ 04Control_Flow/04loops.js
@@ -32,23 +32,40 @@
 
 // break and continue
 
-for (let index = 1; index <= 20; index++) {
+// for (let index = 1; index <= 20; index++) {
 
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-    console.log(`value of i is ${index}`);
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
+
+//+++++++++++++++While and Do While loop++++++++++++++++++
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of Index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["Flash", "Batman", "Superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
 }
 
+//++++++++++++Do While Loop
 
+let score = 11
 
-
-
-
-
-
-
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
 
 
 
