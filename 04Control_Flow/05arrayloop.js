385718a682726f6c8b4dfa7ed610227f6396cd96
sherlockH221B <tusharyeolekar98@gmail.com>
2024-09-02 12:50:18 +0530

    Aarry Loops starting from today

---
+++ 04Control_Flow/05arrayloop.js
@@ -0,0 +1,21 @@
+//++++++++++++++For of Loop 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello World"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+    
+}
+
+//+++++++++++++++ Maps 
+
+const map = new Map()
+map. 
